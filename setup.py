783a1e9ff9ca6a2deb961f5e3b8d4cff69d562ce
John McKenzie <jmckind@gmail.com>
2016-05-22 18:38:11 -0500

    Add setup metadata and create countdown script

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from distutils.core import setup
 
 setup(
     name='timer_clients',
-    version='0.0.1',
+    version='0.1.3',
     description='Python package that provides timer utility commands.',
+    license='MIT',
     author='John McKenzie',
     author_email='jmckind@gmail.com',
     url='https://github.com/jmckind/timer-clients',
     packages=['timer_clients'],
+    scripts=['scripts/countdown'],
 )
