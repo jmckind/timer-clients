9681555f50ec1cc42b4d0a42f058f95b1a93f8fe
John McKenzie <jmckind@gmail.com>
2016-05-22 17:27:49 -0500

    Add distutils setup configuration

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='timer_clients',
+    version='0.0.1',
+    description='Python package that provides timer utility commands.',
+    author='John McKenzie',
+    author_email='jmckind@gmail.com',
+    url='https://github.com/jmckind/timer-clients',
+    packages=['timer_clients'],
+)
